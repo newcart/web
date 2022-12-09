48f4e45c8bbe79b193fb041956360bb985c12500
tulparstudyo <teknovipyazilim@gmail.com>
2022-12-09 19:04:47 +0300

    webservis request güncelleme

--- app/Helpers/WebServiceHelper.php
+++ app/Helpers/WebServiceHelper.php
@@ -1,7 +1,7 @@
 <?php
 use Illuminate\Support\Facades\Http;
 class WebService{
-    protected $server = 'https://newcart.akilliphone.com/webservice/';
+    const WEBSERVICE_SERVER = 'https://newcart.akilliphone.com/webservice/';
     protected $userName = '';
     protected $userPassword = '';
     public static function auth_login(){
@@ -11,41 +11,56 @@ class WebService{
 
     }
     public static function home_main_slider(){
-        self::request('home/main-slider');
+        return self::request('home/main-slider');
     }
     public static function section_category($number){
-
+        return self::request('home/section-category'.$number);
     }
     public static function section_grid($number){
-
+        return self::request('home/section-grid'.$number);
     }
     public static function section_carousel($number){
-
+        return self::request('home/section-carousel'.$number);
     }
     public static function favorite_brands(){
-
+        return self::request('home/favorite-brands');
     }
     private static function request($endpoint, $data=[], $method='GET', $token_required=false){
-        $request = new Http();
-        $request::accept('application/json');
-        if($token_required){
-            $request::withHeaders([
-                'X-First' => 'foo',
-                'X-Second' => 'bar'
-            ]);
-        }
-        if($method=='GET'){
-            $response = $request::get($endpoint);
-        }elseif($method=='POST'){
-            $response = $request::post($endpoint, $data);
-        }elseif($method=='PUT'){
-            $response = $request::put($endpoint, $data);
-        }elseif($method=='DELETE'){
-            $response = $request::delete($endpoint, $data);
-        } else {
+        $result = [];
+        try{
+            $url = self::WEBSERVICE_SERVER.$endpoint;
+            $request = new Http();
+            $request::accept('application/json');
+            if($token_required){
+                $request::withHeaders([
+                    'X-First' => 'foo',
+                    'X-Second' => 'bar',
+                    'Cache-Control', 'no-cache'
+                ]);
+            }
+            if($method=='GET'){
+                $response = $request::get($url);
+            }elseif($method=='POST'){
+                $response = $request::post($url, $data);
+            }elseif($method=='PUT'){
+                $response = $request::put($url, $data);
+            }elseif($method=='DELETE'){
+                $response = $request::delete($url, $data);
+            } else {
+
+            }
+            $body = $response->body();
+            if($body){
+                $json = json_decode($body, 1);
+                if($json['status']){
+                    $result = $json['data'];
+                } else{
+                    $result = [];
+                }
+            }
+        } catch (\Exception $ex){
 
         }
-        dd($response);
-        return $response;
+        return $result;
     }
 }
